46cd48c1e48fccc50f30276f5c22e95676d8c7a5
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-07-23 11:25:23 +0300

    1. Code cleanup.     2. IAutofacStartup interface.

---
+++ test/TauCode.WebApi.Host.Test/DummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.WebApi.Host.Test
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass("todo: remove when real tests are ready");
+        }
+    }
+}
