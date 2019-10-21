ffb7f1a90962f6e6974c3be5dccb70884343f0ce
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-10-21 15:05:43 +0300

    Added first meaningful ut.

---
+++ tests/TauCode.WebApi.Host.Tests.App/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TauCode.WebApi.Host.Tests.App.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("conflict-exception")]
+        public IActionResult ConflictException()
+        {
+            var exception = new InvalidOperationException("Wrong operation.");
+            return this.ConflictError(exception);
+        }
+    }
+}
---
+++ tests/TauCode.WebApi.Host.Tests/WebApiHostExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace TauCode.WebApi.Host.Tests
+{
+    [TestFixture]
+    public class WebApiHostExtensionsTests
+    {
+        private HttpClient _httpClient;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var factory = new Factory();
+            _httpClient = factory.CreateClient();
+
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void ConflictError_ExceptionWithoutCode_ReturnsValidConflictResponse()
+        {
+            // Arrange
+
+
+            // Act
+            var result = _httpClient.GetAsync("conflict-exception").Result;
+
+            // Assert
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.Conflict));
+            Assert.That(result.Headers.GetValues("X-Payload-Type").Single(), Is.EqualTo("Error"));
+
+            var error = result.Content.ReadAsAsync<ErrorDto>().Result;
+            Assert.That(error.Code, Is.EqualTo(typeof(InvalidOperationException).FullName));
+            Assert.That(error.Message, Is.EqualTo("Wrong operation."));
+        }
+    }
+}
