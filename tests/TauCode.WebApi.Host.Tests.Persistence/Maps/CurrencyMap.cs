7b5a17ad9a59c21dfe17320bcc82773d5ed9f02a
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-11-01 14:05:13 +0200

    Added test persistence project.

---
+++ tests/TauCode.WebApi.Host.Tests.Persistence/Maps/CurrencyMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using TauCode.WebApi.Host.Tests.Domain.Currencies;
+
+namespace TauCode.WebApi.Host.Tests.Persistence.Maps
+{
+    public class CurrencyMap : ClassMap<Currency>
+    {
+        public CurrencyMap()
+        {
+            this.Id(x => x.Id);
+            this.Map(x => x.Code);
+            this.Map(x => x.Name);
+            this.Map(x => x.IsAvailableToUsers);
+        }
+    }
+}
