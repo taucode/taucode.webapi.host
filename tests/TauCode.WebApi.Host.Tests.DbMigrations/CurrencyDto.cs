a3d62ebaa02feba9fc1f8a39ce28723beb766ebb
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-11-01 14:09:00 +0200

    Added db migrations test project.

---
+++ tests/TauCode.WebApi.Host.Tests.DbMigrations/CurrencyDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TauCode.WebApi.Host.Tests.DbMigrations
+{
+    public class CurrencyDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool InitialIsAvailableToUsers { get; set; }
+        public decimal InitialRate { get; set; }
+    }
+}
---
+++ tests/TauCode.WebApi.Host.Tests.DbMigrations/M0_Baseline.cs
@@ -0,0 +1,51 @@
+﻿using FluentMigrator;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TauCode.Utils.Extensions;
+
+namespace TauCode.WebApi.Host.Tests.DbMigrations
+{
+    [Migration(0)]
+    public class M0_Baseline : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            this.Create.Table("currency")
+                .WithColumn("id")
+                    .AsGuid()
+                    .NotNullable()
+                    .PrimaryKey("PK_currency")
+                .WithColumn("code")
+                    .AsAnsiString()
+                    .NotNullable()
+                    .Unique("UX_currency_code")
+                .WithColumn("name")
+                    .AsString()
+                    .NotNullable()
+                .WithColumn("is_available_to_users")
+                    .AsBoolean()
+                    .NotNullable();
+
+            this.InsertCurrencies();
+        }
+
+        private void InsertCurrencies()
+        {
+            var json = this.GetType().Assembly.GetResourceText("currencies.json", true);
+            var currencies = JsonConvert.DeserializeObject<List<CurrencyDto>>(json);
+
+            foreach (var currency in currencies)
+            {
+                var row = new
+                {
+                    id = currency.Id,
+                    code = currency.Code,
+                    name = currency.Name,
+                    is_available_to_users = currency.InitialIsAvailableToUsers,
+                };
+
+                this.Insert.IntoTable("currency").Row(row);
+            }
+        }
+    }
+}
